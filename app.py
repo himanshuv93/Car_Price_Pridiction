04552b37725ebba84f89e1ffd6556082e8e561f3
Himanshu Verma <129640053+himanshuv93@users.noreply.github.com>
2023-07-15 14:03:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 
 
 # Load the trained model
-model = joblib.load('ExtraTreesRegressorModel.joblib')
+model = joblib.load('ExtraTreesRegressorModel.pkl')
 # Load dataset
 df = pd.read_csv('comapany.csv')
 
